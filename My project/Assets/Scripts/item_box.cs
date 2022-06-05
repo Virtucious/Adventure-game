ea6034f1c0921546b08f7bd9d919a7ad48ddf951
Virtucious <aman.rahman.0309@gmail.com>
2022-06-05 18:13:24 +0530

    Added Collidable, item_box, collectable

---
+++ My project/Assets/Scripts/item_box.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item_box : Collectable
+{
+    protected override void OnCollide(Collider2D coll)
+    {
+        base.OnCollide(coll);
+    }
+}
